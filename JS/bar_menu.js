f6d98ae83267e8e971147d6c59cfb9c170f09114
lingjian99 <lingjian99@yeah.net>
2022-04-08 12:31:14 -0700

    fixed navigatorbar

---
+++ JS/bar_menu.js
@@ -0,0 +1,25 @@
+function barMenu(){
+    document.write(
+        '<div class="navbar fixed-top navbar-expand-lg navbar-light bg-warning">\
+        <a><i class="fa-solid fa-burger" style="font-size: 24px; color: rgb(200, 100, 20); text-align: left;"></i> &nbsp;<i class="fa-solid fa-mug-hot" style="font-size: 20px; color: rgb(34, 104, 110); align-self: left;"></i></a>\
+            <a class="navbar-brand" href="#">&nbsp &nbsp无处安放的猪</a>\
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
+                <span class="navbar-toggler-icon"></span>\
+            </button>\
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">\
+                <ul class="navbar-nav ml-auto topnav">\
+                    <li class="nav-item active">\
+                        <a class="nav-link" href="index.html"><i class="fa-solid fa-house-chimney-window"></i> Home </a>\
+                    </li>\
+                    <li class="nav-item">\
+                        <a class="nav-link" href="#"><i class="fa-regular fa-envelope"></i> Contact</a>\
+                    </li>\
+                </ul>\
+            </div>\
+        </div>'
+    )
+}
+
+function comingSoon(){
+    alert('Coming soon...');
+}

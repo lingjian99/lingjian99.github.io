9ccd6ec0a7dc7ecaa9b2b2d16c355c9e24c075af
lingjian99 <lingjian99@yeah.net>
2022-09-27 23:43:38 -0700

    111

--- JS/blog_chief.js
+++ JS/blog_chief.js
@@ -1,10 +1,10 @@
 
 function blog_chief_large(title)
 {
-    let str ='<div class="div1 col-lg-3 col-md-4 col-sm-6 col-xs-12 dash">  <div><a href="./blogs/'
-        + title
-        + '/main.html"><img class="dash" src="./blogs/'
-        + title
-        + '/images/cover.jpg" class="img-responsive" alt="No image"></a></div></div>';
+    let str = '<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"> <a href="blogs/'
+        +title
+        +'/main.html"><img class="dash" src="blogs/'
+        +title
+        +'/images/cover.jpg" class="img-responsive" alt="No image"></a></div>';
     document.write(str);
 }

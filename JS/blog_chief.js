d4ca4e9f6df170d0a5435a6eea465b41003eeb31
lingjian99 <lingjian99@yeah.net>
2022-10-05 17:45:05 -0700

    111

--- JS/blog_chief.js
+++ JS/blog_chief.js
@@ -3,9 +3,20 @@ function blog_chief_large(title,summary)
 {
     let str = '<div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12" > <a href="blogs/'
         +title
-        +'/main.html"><img class="dash" src="blogs/'
+        +'/main.html"><img src="blogs/'
         +title
         +'/images/cover.jpg" class="img-responsive" alt="No image"></a>'
         +'<div style="margin-top:6px;margin-bottom:10px;font-size:14px;">'+summary+'</div></div>';
+    document.write(str);
+}
+
+function blog_chief_large1(name, title,summary)
+{
+    let str = '<div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12" ><div style=""> <a href="blogs/'
+        +name
+        +'/main.html"><img class="dash" src="blogs/'
+        +name
+        +'/images/cover.jpg" class="img-responsive" alt="No image"></a>'
+        +'<div style="margin-top:6px;margin-bottom:10px;font-size:14px;">'+summary+'</div></div><div style="z-index:100;"><a href="blogs/'+name+'/main.html">'+title+'</a></div></div>';
     document.write(str);
 }
